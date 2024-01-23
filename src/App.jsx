fd28b0670a5024cc3eff5116a871bee4cb1f407d
Daniel Bautista <dbautista9015@gmail.com>
2024-01-23 10:11:15 -0800

    Configure eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,36 @@ import { CORE_CONCEPTS } from './data';
 import Header from './components/Header';
 import CoreConcept from './components/CoreConcepts';
 
-
 function App() {
   return (
     <div>
       <Header />
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-      <ul>
-        <CoreConcept 
-          title={CORE_CONCEPTS[0].title}
-          description={CORE_CONCEPTS[0].description}
-          image={CORE_CONCEPTS[0].image}
-      />
-        <CoreConcept 
-          title={CORE_CONCEPTS[1].title}
-          description={CORE_CONCEPTS[1].description}
-          image={CORE_CONCEPTS[1].image}
-      />
-        <CoreConcept 
-          title={CORE_CONCEPTS[2].title}
-          description={CORE_CONCEPTS[2].description}
-          image={CORE_CONCEPTS[2].image}
-      />
-        <CoreConcept 
-          title={CORE_CONCEPTS[3].title}
-          description={CORE_CONCEPTS[3].description}
-          image={CORE_CONCEPTS[3].image}
-      />
-      </ul>
-      </section>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,9 @@ function Header() {
     <header>
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any 
-        app you are going to build!
-      </p>
+      <p>{description} React concepts you will need for almost any app you are going to build!</p>
     </header>
   );
-
 }
 
 export default Header;
