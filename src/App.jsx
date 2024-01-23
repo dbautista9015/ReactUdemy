2c791303ea9457d68bd4a97e47f9156c0b2559de
Daniel Bautista <dbautista9015@gmail.com>
2024-01-22 21:24:34 -0800

    Add data file and use object destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -22,12 +23,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
     return (
         <li>
-            <img src={props.image} alt={props.title} />
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
         </li>
     );
 }
@@ -41,9 +42,24 @@ function App() {
         <h2>Core Concepts</h2>
       <ul>
         <CoreConcept 
-        title="Components"
-        description="The Core UI building block."
-        image={componentsImg}
+        title={CORE_CONCEPTS[0].title}
+        description={CORE_CONCEPTS[0].description}
+        image={CORE_CONCEPTS[0].image}
+      />
+        <CoreConcept 
+        title={CORE_CONCEPTS[1].title}
+        description={CORE_CONCEPTS[1].description}
+        image={CORE_CONCEPTS[1].image}
+      />
+        <CoreConcept 
+        title={CORE_CONCEPTS[2].title}
+        description={CORE_CONCEPTS[2].description}
+        image={CORE_CONCEPTS[2].image}
+      />
+        <CoreConcept 
+        title={CORE_CONCEPTS[3].title}
+        description={CORE_CONCEPTS[3].description}
+        image={CORE_CONCEPTS[3].image}
       />
       </ul>
       </section>
